72681fddf63b7f5e36f0f8c379070982e83105ca
chail233 <1744315424@qq.com>
2026-08-13 22:14:28 +0800

    feat:台词

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -1,12 +1,23 @@
 import {sendGroupMsg} from "../websocket/act.js";
 import config from "../config.js";
 import needRepeat from "./repeater.js";
-export default function(event, socket) {
+import getSentence from "./sentence.js";
+const sentence_command = "来句台词";
+const sentence_fail = "急什么，慢点发";
+
+export default async function(event, socket) {
     if(event.post_type!=="message"){return;}
     if(event.message_type==="group" && event.group_id==config.targetGroupId){
         let text = extractText(event.message);
         console.log("received group message:", text);
-        if(needRepeat(text)){
+
+        if(text.toString()===sentence_command){
+            const sentence = await getSentence();
+            if(sentence) sendGroupMsg(socket, event.group_id, sentence);
+            else sendGroupMsg(socket, event.group_id, sentence_fail);
+        }
+
+        if(text.toString()!== sentence_command && needRepeat(text)){
             sendGroupMsg(socket, event.group_id, text);
         }
     }
